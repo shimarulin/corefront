e195205c429f34a157be5e6b7f84b29a9dd36590
Vyacheslav Shimarulin <shimarulin@gmail.com>
2024-04-03 09:14:33 +0300

    feat: make cx global

--- tools/cli/README.md
+++ tools/cli/README.md
@@ -48,3 +48,23 @@ cx
 ```sh
 pnpm uninstall --global @corefront/cli
 ```
+
+## Common issues
+
+### WARN link:...corefront/tools/cli has no binaries
+
+`pnpm link --global` show warning:
+
+```
+WARN  link:/home/username/corefront/tools/cli has no binaries
+```
+
+Don't worry, everything just works. See https://github.com/pnpm/pnpm/issues/4761 for updates.
+
+### `cx` is stil present after `pnpm uninstall --global @corefront/cli`
+
+Sometime this happens, use:
+
+```sh
+rm -f "$(pnpm bin -g)/cx"
+```
--- tools/cli/src/cli.ts
+++ tools/cli/src/cli.ts
@@ -1,3 +1,15 @@
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url';
+
 export const run = async (): Promise<void> => {
-  console.log('Hello from CLI!')
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const projectRoot = resolve(__dirname, '../../../')
+
+  console.log(projectRoot)
+  // const pkg = await import(resolve(__dirname, '../package.json'), {with: {type: 'json'}})
+
+  // console.log(pkg)
+  console.log('Hello from upd CLI!')
 }
